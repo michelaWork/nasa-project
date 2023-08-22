a1a523e4f68dfef9d109858564f2a8dd5bd26a11
michelaWork <101396014+michelaWork@users.noreply.github.com>
2023-08-22 17:31:52 +0200

    client & server on one port to deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 //using express as a middleware
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
@@ -11,6 +12,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
